06d03314224f31cdef7f174f78df91391ae0e38c
Myeonggyun Han <mhan@knu.ac.kr>
2025-11-21 15:32:27 +0900

    update

--- test_mini-shell-2.sh
+++ test_mini-shell-2.sh
@@ -86,8 +86,13 @@ if [[ -z "$CHILD_PID" ]]; then
   exit 1
 fi
 
+kill -20 "$SHELL_PID"
+kill -20 "$CHILD_PID"
+kill -STOP "$SHELL_PID"
 kill -TSTP "$SHELL_PID"
 kill -TSTP "$CHILD_PID"
+kill -STOP "$SHELL_PID"
+kill -STOP "$CHILD_PID"
 sleep 1
 
 STATE=$(ps -o stat= -p "$CHILD_PID" 2>/dev/null)
